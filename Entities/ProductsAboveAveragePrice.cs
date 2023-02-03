91f6d865fdb3f3cf0a2229aec5438ba4f4ae6b76
in2dm3-v <in2dm3-v@INFHERBE14>
2023-02-03 16:15:56 +0100

    p

---
+++ Entities/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reto2eSge_3__.Entities
+{
+    public partial class ProductsAboveAveragePrice
+    {
+        public string ProductName { get; set; } = null!;
+        public decimal? UnitPrice { get; set; }
+    }
+}
